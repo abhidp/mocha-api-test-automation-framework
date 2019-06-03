605dc7432f5460780fa2741f06c3381b0eb276b3
abhidp <abhi358@gmail.com>
2019-06-03 20:01:00 +1000

    get User by Id

--- specs/getUsers.spec.js
+++ specs/getUsers.spec.js
@@ -3,17 +3,43 @@ import axios from 'axios';
 import { expect } from 'chai';
 const token = process.env.TOKEN;
 const baseUrl = process.env.BASE_URL;
+const headers = { Authorization: `Bearer ${token}` };
 
-describe('Users', async () => {
+describe('Test Users Endpoint', async () => {
   it('GET all users', async () => {
     const response = await axios({
       method: 'GET',
-      headers: { Authorization: `Bearer ${token}` },
+      headers,
       url: `${baseUrl}/users`,
     });
 
-    console.log('RESPONSE===', response.data.result[0]);
+    expect(response.status).to.equal(200);
+    expect(response.headers['content-type']).to.contain('application/json');
+    expect(response.data).to.be.an('object');
+    expect(response.data.result).to.be.an('array');
+  });
+
+  it('GET users by Id', async () => {
+    const response = await axios({
+      method: 'GET',
+      headers,
+      url: `${baseUrl}/users/1744`,
+    });
 
     expect(response.status).to.equal(200);
+    expect(response.headers['content-type']).to.contain('application/json');
+    expect(response.data).to.be.an('object');
+    expect(response.data.result).to.be.an('object');
+    expect(response.data.result).to.include.keys(
+      'id',
+      'first_name',
+      'gender',
+      'dob',
+      'email',
+      'phone',
+      'website',
+      'address',
+      'status'
+    );
   });
 });
